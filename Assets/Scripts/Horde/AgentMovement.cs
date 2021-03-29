8bd14d61b888c806e0a866887f688be6bac54325
Director <dir.katebi@gmail.com>
2021-03-30 03:48:02 +0430

    Character Animation and Movement

--- Assets/Scripts/Horde/AgentMovement.cs
+++ Assets/Scripts/Horde/AgentMovement.cs
@@ -7,16 +7,40 @@ public class AgentMovement : MonoBehaviour
 {
     Vector2 m_targetPos;
     NavMeshAgent2D pathfinder;
+    public float vicinity;
+    Animator animator;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
 
     void Awake(){
         pathfinder = GetComponent<NavMeshAgent2D>();
     }
-
+    
     public void Initialize(int moveSpeed){
         pathfinder.speed = moveSpeed;
     }
 
     public void SetDestination(Vector2 pos){
         pathfinder.destination = pos;
+    }
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        pathfinder = GetComponent<NavMeshAgent2D>();
+    }
+    void Update()
+    {
+        if (pathfinder == null || pathfinder.remainingDistance < vicinity)
+        {
+            animator.SetBool("isMoving", false);
+        }
+        else
+        {   
+            Vector3 direction = (pathfinder.destination - rb.position).normalized;
+            sr.flipX = direction.x < 0 ? true : false;
+            animator.SetBool("isMoving", true);
+        }
     }
 }
